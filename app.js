09495ff99fab29c8b28d797f697ef82c96cd4c90
Richard <richardcgayle@gmail.com>
2021-07-06 20:23:00 -0400

    additional features to hamburger menu

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const navMobile = function() {
     const burger = document.querySelector('.burger-menu');
     const nav = document.querySelector('.standard-ul');
+    const burgerLineOne = document.querySelector('.line-1');
+    const burgerLineTwo = document.querySelector('.line-2');
+    const burgerLineThree = document.querySelector('.line-3');
 
     burger.addEventListener('click', function() {
         nav.classList.toggle('burger-show');
+        burgerLineOne.classList.toggle('line-1-active');
+        burgerLineTwo.classList.toggle('line-2-active');
+        burgerLineThree.classList.toggle('line-3-active');
     });
 }
 
